69bf1f59da7775b2d522c402ba164931701b4b3a
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-07 02:39:12 +0530

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,7 @@ $(function () {
       // grecaptcha.reset();
       // }
       // }
-      var v = document.getElementById("mainvideo");
-      v.playbackRate = 0.8;
+
 
       $('.form-default .form-control').blur(function () {
             if (this.value) {
