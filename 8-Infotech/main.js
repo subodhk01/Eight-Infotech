e62508c9fdc9033802dfb118c2b994a99828f745
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-15 02:36:13 +0530

    added fake 8 infotech

---
+++ 8-Infotech/main.js
@@ -0,0 +1,7 @@
+function on(element) {
+    document.getElementById(element).style.display = "block";
+  }
+  
+  function off(element) {
+    document.getElementById(element).style.display = "none";
+  }
