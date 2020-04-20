addc1ba5c71a5b68762fd3bf8564da815d0adb0d
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-20 23:26:10 +0530

    sendmail.php syntax fixed

--- sendmail.php
+++ sendmail.php
@@ -5,7 +5,7 @@ $email=$_POST['email'];
 $subject=$_POST['subject'];
 $message=$_POST['message'];
 
-$queryData = "Hi Deepesh,\n You have one query.\n Name: "+$first-name+" "+$last-name+",\n Email: "+$email+",\n Subject:"+$subject+",\n Message: "+message+".";
+$queryData = "Hi Deepesh,\n You have one query.\n Name: "+$first-name+" "+$last-name+",\n Email: "+$email+",\n Subject:"+$subject+",\n Message: "+$message+".";
 $subjectLine = "Query for Eight Infotech"
 mail("deepeshsonaniya@gmail.com", $subjectLine, $queryData);
 ?>
